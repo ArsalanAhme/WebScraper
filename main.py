d3d03d1107f66985dbd56534ccc4c0a8fb600e6e
Arsalan <zkac440@live.rhul.ac.uk>
2026-01-21 16:22:06 +0000

    Fixed error in which csv was not being accessed properly, and scraper was not using updated instance of the website

--- main.py
+++ main.py
@@ -2,39 +2,48 @@ import csv,time,requests,os
 from bs4 import BeautifulSoup
 
 url = "https://news.ycombinator.com/"
-response = requests.get(url)
-
-print(f"status code {response.status_code}")
-
-soup = BeautifulSoup(response.text, 'html.parser')
-
-
-things = soup.find_all(class_= "titleline",limit = 5)
 filename = 'Results.csv'
 seen_urls = set()
 
 if not os.path.exists(filename):
         with open(filename,'w') as file:
-                file.writelines(["Rank","Headline","Link"])   
+                csvwriter = csv.writer(file)
+                csvwriter.writerow(["Rank","Headline","Link"])   
 else:
         print("file already exists, skipping header")
         
 while True:
-        with open(filename,'a') as csvfile:
-                csvwriter = csv.writer(csvfile)
-                for i, thing in enumerate(things, start=1):
-                        article_url = thing.find('a')['href']
-                        if article_url not in seen_urls:
-                                seen_urls.add(article_url)
-                                new_part = thing.text.rsplit(' (',1)
-                                headline = new_part[0]
-                                print(f"Rank {i}.{thing.text}")
-                                csvwriter.writerow([i,headline,article_url])
-                                print("successful scrape")
-                        else:
-                                print("Skipped, article already exists in database")
-                waiting_time = 10
-                print(f"waiting {waiting_time} seconds.")
-                time.sleep(waiting_time)
+        try:
+                response = requests.get(url)
+                soup = BeautifulSoup(response.text, 'html.parser')
+                things = soup.find_all(class_= "titleline",limit = 20)
+                print(f"status code {response.status_code}")
+                new_articles = 0
+
+                with open(filename,'a') as csvfile:
+                        csvwriter = csv.writer(csvfile)
+                        for i, thing in enumerate(things, start=1):
+
+                                article_url = thing.find('a')['href']
+                                if article_url not in seen_urls:
+                                        seen_urls.add(article_url)
+                                        new_part = thing.text.rsplit(' (',1)
+                                        headline = new_part[0]
+
+                                        csvwriter.writerow([i,headline,article_url])
+                                        print(f"New: Rank {i} - Headline {headline}")
+                                        new_articles+=1
+                                else:
+                                        pass
+                        
+                        if new_articles == 0:
+                                print("No New Articles were found")
+
+                        waiting_time = 10
+                        print(f"waiting {waiting_time} seconds.")
+                        time.sleep(waiting_time)
+        except Exception as e:
+                print(f"error occured {e}")
+                time.sleep(10)
 
             
