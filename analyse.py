3e1d5fd1fb353201fe3215c6fc38f211ca3f3a01
Arsalan <zkac440@live.rhul.ac.uk>
2026-01-30 18:27:50 +0000

    filtering out common words to make leaderboard for buzzwords

--- analyse.py
+++ analyse.py
@@ -5,8 +5,25 @@ connection = sqlite3.connect("news.db")
 df = pd.read_sql_query("SELECT * FROM stories",connection)
 connection.close()
 
-print("dataframe shape, rows, columns")
-print(df.shape)
+df["domain"] = df['link'].str.extract(r'://(.*?)/', expand=False)
 
-print("first few rows")
-print(df.head())
+source_count = df['domain'].value_counts()
+
+print(source_count.head(5))
+
+# print("dataframe shape, rows, columns")
+# print(df.shape)
+
+# print("first few rows")
+# print(df.head())
+
+
+print("\ntop words within headlines")
+stop_words = ['the', 'a', 'to', 'of', 'in', 'and', 'is', 'for', 'on', 'with', 'by', 'at', 'from', 'it']
+
+
+all_words= df['headline'].str.lower().str.split().explode()
+interesting_words = all_words[~all_words.isin(stop_words)]
+
+
+print(interesting_words.value_counts().head(5))
